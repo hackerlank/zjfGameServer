c05f4b91b89cf187f6c64e4474b68fb86241be29
wcy <1101697681@qq.com>
2016-04-29 19:02:43 +0800

    add Kitchen

--- src/byCodeGame/game/entity/bo/Kitchen.java
+++ src/byCodeGame/game/entity/bo/Kitchen.java
@@ -1,6 +1,10 @@
 package byCodeGame.game.entity.bo;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import byCodeGame.game.entity.bo.base.RoleComponent;
+import byCodeGame.game.entity.po.HearthSpace;
 
 /**
  * 厨房
@@ -9,5 +13,19 @@ import byCodeGame.game.entity.bo.base.RoleComponent;
  *
  */
 public class Kitchen extends RoleComponent {
+	private Map<Byte, HearthSpace> hearthSpace = new HashMap<>();
 
+	private String hearthSpaceStr;
+
+	public Map<Byte, HearthSpace> getHearthSpace() {
+		return hearthSpace;
+	}
+
+	public void setHearthSpaceStr(String hearthSpaceStr) {
+		this.hearthSpaceStr = hearthSpaceStr;
+	}
+
+	public String getHearthSpaceStr() {
+		return hearthSpaceStr;
+	}
 }
---
+++ src/byCodeGame/game/entity/po/HearthSpace.java
@@ -0,0 +1,5 @@
+package byCodeGame.game.entity.po;
+
+public class HearthSpace {
+	private int hearth;
+}
